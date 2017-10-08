e37fad52be9207f49f63c47cf6f67ec103808d5d
cexbrayat <cedric@ninja-squad.com>
2017-10-07 22:21:19 -0400

    chore: version 1.3.0-beta.0

--- ponyracer/e2e/app.e2e-spec.ts
+++ ponyracer/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { PonyracerPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('ponyracer App', () => {
-  let page: PonyracerPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new PonyracerPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
 });
