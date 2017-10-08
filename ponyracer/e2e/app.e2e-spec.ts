9778312fa112fd76422b400f891a934936e9b7b4
cexbrayat <cedric@ninja-squad.com>
2017-10-07 22:07:47 -0400

    chore: version 1.1.1

--- ponyracer/e2e/app.e2e-spec.ts
+++ ponyracer/e2e/app.e2e-spec.ts
@@ -7,10 +7,8 @@ describe('ponyracer App', () => {
     page = new PonyracerPage();
   });
 
-  it('should display welcome message', done => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    page.getParagraphText()
-      .then(msg => expect(msg).toEqual('Welcome to app!!'))
-      .then(done, done.fail);
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
 });
