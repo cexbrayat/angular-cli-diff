b59342db9e145dc9586e958734d526a54f74d296
cexbrayat <cedric@ninja-squad.com>
2018-05-30 07:50:43 +0200

    chore: version 6.1.0-beta.0

--- ponyracer/src/app/app.component.spec.ts
+++ ponyracer/src/app/app.component.spec.ts
@@ -13,10 +13,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'ponyracer'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('ponyracer');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
