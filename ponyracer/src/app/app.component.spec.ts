eb3a4a1ce4f564d384aea1621e7e52a159c799f3
cexbrayat <cedric@ninja-squad.com>
2019-10-21 22:02:39 +0000

    chore: version 9.0.0-next.13

--- ponyracer/src/app/app.component.spec.ts
+++ ponyracer/src/app/app.component.spec.ts
@@ -12,20 +12,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'ponyracer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app.title).toEqual('ponyracer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
+    const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('ponyracer app is running!');
   });
 });
