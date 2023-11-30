e34ed87facd0e8c5b0f9371d5e2673cd40794226
cexbrayat <cedric@ninja-squad.com>
2023-11-30 00:31:07 +0000

    chore: version 17.1.0-next.0

--- ponyracer/src/app/app.component.ts
+++ ponyracer/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
