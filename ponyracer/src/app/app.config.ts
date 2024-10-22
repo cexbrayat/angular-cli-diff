bd1ed7ebbc4a8dc899decfb71b13605763280a5d
cexbrayat <cedric@ninja-squad.com>
2024-10-22 00:39:35 +0000

    chore: version 19.0.0-next.12

--- ponyracer/src/app/app.config.ts
+++ ponyracer/src/app/app.config.ts
@@ -4,7 +4,5 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-  provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
