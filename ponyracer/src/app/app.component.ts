173c03241dba421f5c428ebf87c1e3c0ec6de880
cexbrayat <cedric@ninja-squad.com>
2025-03-13 10:11:32 +0000

    chore: version 20.0.0-next.1

--- ponyracer/src/app/app.component.ts
+++ ponyracer/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.ng.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
