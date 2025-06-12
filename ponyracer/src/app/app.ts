51ce3ac8e9380bdb9a12de7ea9630cff387e6c33
cexbrayat <cedric@ninja-squad.com>
2025-06-12 00:47:06 +0000

    chore: version 20.1.0-next.1

--- ponyracer/src/app/app.ts
+++ ponyracer/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'ponyracer';
+  protected readonly title = signal('ponyracer');
 }
