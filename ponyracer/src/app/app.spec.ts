f95f60f458c8b76505fd1f49fcd669930099438c
cexbrayat <cedric@ninja-squad.com>
2025-10-03 14:09:57 +0000

    chore: version 21.0.0-next.6

--- ponyracer/src/app/app.spec.ts
+++ ponyracer/src/app/app.spec.ts
@@ -14,9 +14,9 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should render title',  async () => {
     const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
+    await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ponyracer');
   });
