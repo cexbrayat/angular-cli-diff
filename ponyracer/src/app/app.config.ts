5316086540e6805d8ac71c0495a6701ba52ef86b
cexbrayat <cedric@ninja-squad.com>
2026-08-19 20:14:52 +0000

    chore: version 22.1.5

--- ponyracer/src/app/app.config.ts
+++ ponyracer/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
--- ponyracer/src/app/app.ts
+++ ponyracer/src/app/app.ts
@@ -2,10 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
   imports: [RouterOutlet],
+  selector: 'app-root',
+  styleUrl: './app.css',
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ponyracer');
